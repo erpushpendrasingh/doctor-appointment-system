6770876fafe2f3c47bbcd1ef26acaa77fc2ad923
Pushpendra Singh <erpushpendrasingh8900@gmail.com>
2023-04-15 23:06:55 +0530

    ApplyDictor page

--- client/src/pages/ApplyDoctor.js
+++ client/src/pages/ApplyDoctor.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { Form } from "antd";
 
 const ApplyDoctor = () => {
+     const handleFinish = () => {};
      return (
           <Layout>
-               <h1>Apply Doctor</h1>
+               <h1 className="text-center">Apply Doctor</h1>
+               <Form layout="vertical" onFinish={handleFinish}></Form>
           </Layout>
      );
 };
