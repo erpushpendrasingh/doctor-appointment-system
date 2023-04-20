c53ae05f6567485a41cace8f4f0d26b7dadd57ae
Pushpendra Singh <erpushpendrasingh8900@gmail.com>
2023-04-21 00:34:04 +0530

    doctor page userpage inside admin folder

--- client/src/pages/Admin/Doctors.js
+++ client/src/pages/Admin/Doctors.js
@@ -1,10 +1,66 @@
-import React from "react";
 import Layout from "../../components/Layout";
+import React, { useState } from "react";
+import axios from "axios";
+import { Table } from "antd";
 
 const Doctors = () => {
+     const [doctor, setDoctor] = useState([]);
+     const getDoctors = async () => {
+          try {
+               const res = await axios.get("/api/v1/admin/getAllDoctors", {
+                    headers: {
+                         Authorization: `Bearer ${localStorage.getItem(
+                              "token"
+                         )}`,
+                    },
+               });
+               if (res.data.success) {
+                    setDoctor(res.data.data);
+               }
+          } catch (error) {}
+     };
+     useState(() => {
+          getDoctors();
+     }, []);
+     const columns = [
+          {
+               title: "Name",
+               dataIndex: "name",
+               render: (text, record) => (
+                    <span>
+                         {record.firstName} {record.lastName}
+                    </span>
+               ),
+          },
+          {
+               title: "Status",
+               dataIndex: "status",
+          },
+          {
+               title: "Phone",
+               dataIndex: "phone",
+          },
+
+          {
+               title: "Actions",
+               dataIndex: "actions",
+               render: (text, record) => (
+                    <div className="d-flex">
+                         {record.status === "pending" ? (
+                              <button className="btn btn-success">
+                                   Approve
+                              </button>
+                         ) : (
+                              <button className="btn btn-danger">Reject</button>
+                         )}
+                    </div>
+               ),
+          },
+     ];
      return (
           <Layout>
-               <h4>Doctors</h4>
+               <h4 className="m-3 text-center">Doctors</h4>
+               <Table columns={columns} dataSource={doctor} />
           </Layout>
      );
 };
--- client/src/pages/Admin/User.js
+++ client/src/pages/Admin/User.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../../components/Layout";
+import axios from "axios";
+import { Table } from "antd";
 
 const User = () => {
+     const [users, setUsers] = useState([]);
+     const getUsers = async () => {
+          try {
+               const res = await axios.get("/api/v1/admin/getAllUsers", {
+                    headers: {
+                         Authorization: `Bearer ${localStorage.getItem(
+                              "token"
+                         )}`,
+                    },
+               });
+               if (res.data.success) {
+                    setUsers(res.data.data);
+               }
+          } catch (error) {}
+     };
+     useState(() => {
+          getUsers();
+     }, []);
+     const columns = [
+          {
+               title: "Name",
+               dataIndex: "name",
+          },
+          {
+               title: "Email",
+               dataIndex: "email",
+          },
+          {
+               title: "Doctor",
+               dataIndex: "isDoctor",
+               render: (text, record) => (
+                    <span> {record.isDoctor ? "Yes" : "No"}</span>
+               ),
+          },
+          {
+               title: "Actions",
+               dataIndex: "action",
+               render: (text, record) => (
+                    <div className="d-flex">
+                         <button className="btn btn-danger">Block</button>
+                    </div>
+               ),
+          },
+     ];
      return (
           <Layout>
-               <h4>User List</h4>
+               <h4 className="text-center m-2">User List</h4>
+               <Table columns={columns} dataSource={users} />
           </Layout>
      );
 };
